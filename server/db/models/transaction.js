8e610f0f6fb34a98259c160b6463d596067f6366
Evangelos Vekios <evekios@gmail.com>
2018-10-30 16:02:50 -0400

    completes review and transaction models

--- server/db/models/transaction.js
+++ server/db/models/transaction.js
@@ -2,8 +2,25 @@ const db = require('../db')
 const Sequelize = require('sequelize')
 
 const Transaction = db.define('transaction', {
-  isPurchased: {
-    type: Sequelize.BOOLEAN
+  status: {
+    type: Sequelize.ENUM,
+    values: ['pending', 'purchased', 'shipped'],
+    defaultValue: 'pending'
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    defaultValue: 0,
+    validate: {
+      min: 0.0
+    }
+  },
+  quantity: {
+    type: Sequelize.INTEGER,
+    defaultValue: 1,
+    validate: {
+      min: 1
+    }
   }
 })
 
