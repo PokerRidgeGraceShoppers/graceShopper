8e53dcb2585a42c3dd57ce019cf42bdc5569906c
Evangelos Vekios <evekios@gmail.com>
2018-10-30 15:48:15 -0400

    creates preliminary test specs

---
+++ server/db/models/product.spec.js
@@ -0,0 +1,25 @@
+const {expect} = require('chai')
+const db = require('../index')
+const Product = db.model('product')
+
+describe('Product model', () => {
+  beforeEach(() => {
+    return db.sync({force: true})
+  })
+
+  describe('testing product', () => {
+    let aRock
+    beforeEach(async () => {
+      aRock = await Product.create({
+        title: 'A rock',
+        category: 'Igneous',
+        price: 9.99,
+        description: "it's a hecking rock",
+        availability: false
+      })
+    })
+    it('works', () => {
+      expect(aRock.availability).to.be.false
+    })
+  })
+})
