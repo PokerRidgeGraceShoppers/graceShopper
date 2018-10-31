c1db092aba0663508a96877a196821d90f529b9d
Ryan <rynpowers@gmail.com>
2018-10-30 20:55:15 -0400

    file restructure for client folder

--- client/store/index.js
+++ client/store/index.js
@@ -1,14 +1,12 @@
-import {createStore, combineReducers, applyMiddleware} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import user from './user'
+import reducers from './reducers'
 
-const reducer = combineReducers({user})
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
-const store = createStore(reducer, middleware)
+const store = createStore(reducers, middleware)
 
 export default store
-export * from './user'
