956372794a604f07bbdae1e7f1298f1c456b1fe9
Evangelos Vekios <evekios@gmail.com>
2018-11-02 13:09:16 -0400

    adds cart and products links to navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -3,26 +3,14 @@ import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store/actions/users'
-import Dropdown from './common/Dropdown'
 
-//search term
-//category term
-
-const Navbar = ({handleClick, isLoggedIn, products}) => (
+const Navbar = ({handleClick, isLoggedIn}) => (
   <div style={{position: 'relative'}}>
     <h1>PokerRidgeGraceShopper</h1>
     <nav>
-      <div>
-        <Dropdown>
-          <ul>Category 1</ul>
-          <ul>Category 2</ul>
-          <ul>Category 3</ul>
-          <ul>Category 4</ul>
-          <ul>Category 5</ul>
-          <ul>Category 6</ul>
-        </Dropdown>
-
-        <input placeholder="a thing" />
+      <div style={{display: 'flex', flexDirection: 'row-reverse'}}>
+        <Link to="/cart">Cart</Link>
+        <Link to="/products">See all Products</Link>
       </div>
       {isLoggedIn ? (
         <div>
@@ -37,7 +25,6 @@ const Navbar = ({handleClick, isLoggedIn, products}) => (
           {/* The navbar will show these links before you log in */}
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
-          <Link to="/products">See all Products</Link>
         </div>
       )}
     </nav>
@@ -50,8 +37,7 @@ const Navbar = ({handleClick, isLoggedIn, products}) => (
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.user.id,
-    products: state.products.products
+    isLoggedIn: !!state.user.id
   }
 }
 
@@ -72,8 +58,3 @@ Navbar.propTypes = {
   handleClick: PropTypes.func.isRequired,
   isLoggedIn: PropTypes.bool.isRequired
 }
-
-//          {Object.keys(products).map(productId => {
-//   const {category} = products[productId]
-//   return <li key={productId}>{category}</li>
-// })}
