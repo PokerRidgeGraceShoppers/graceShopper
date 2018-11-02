9edd2c5f614c06747cf664bdf57291ef4b62a2e7
Ryan <rynpowers@gmail.com>
2018-11-02 00:01:08 -0400

    changed load in component mount to routes

--- client/app.js
+++ client/app.js
@@ -1,23 +1,14 @@
 import React from 'react'
-import {withRouter} from 'react-router-dom'
 import {Navbar} from './components'
 import Routes from './routes'
-import {connect} from 'react-redux'
-import {fetchProducts} from './store/actions/products'
-import {fetchCart} from './store/actions/cart'
 
-class App extends React.Component {
-  componentDidMount() {
-    this.props.fetchProducts().then(() => this.props.fetchCart())
-  }
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <Routes />
-      </div>
-    )
-  }
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Routes />
+    </div>
+  )
 }
 
-export default withRouter(connect(null, {fetchProducts, fetchCart})(App))
+export default App
